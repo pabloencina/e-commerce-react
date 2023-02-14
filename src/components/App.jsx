5a376a450db4844c5879422f63bb0bbbc093897f
Pablo <encina.pd@gmail.com>
2023-02-14 08:45:57 -0300

    Se crea un nuevo componente ProductToShowOnSale

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,20 @@
 import NavBar from "./navbar/NavBar";
-import ProductCard from "./cards/ProductCard";
+//import ProductCard from "./cards/ProductCard";
 import React from "react";
 import useAccountant from "./hooks/useAccountant";
 import Footer from "./Footer";
 import CarouselImages from "./carousel/CarouselImages";
 import { Box } from "@mui/system";
 import SearchAutocomplete from "./navbar/SearchAutocomplete";
-import dataArray from "./ejemplo";
-import { Grid } from "@mui/material";
+//import dataArray from "./ejemplo";
+//import { Grid } from "@mui/material";
 import Información from "./Informacion";
 //import ProductCardById from "./cards/ProductCardById";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 //import ItemCount from "./cards/ItemCount";
 import Register from "./register/Register";
 import Login from "./login/Login";
+import ProductsToShowOnSale from "./cards/ProductsToShowOnSale";
 // import Nosotros from "./Nosotros";
 //import { collection, getDocs } from "firebase/firestore";
 //import { useEffect } from "react";
@@ -32,13 +33,7 @@ function App() {
   //   getUsuarios();
   // }, []);
 
-  const { count, increase } = useAccountant(0);
-
-  const onProductAdded = (quantity) => {
-    increase(quantity);
-  };
-
-  const productsToShow = dataArray.filter((card) => card.sale);
+  const { count } = useAccountant(0);
 
   return (
     <BrowserRouter>
@@ -51,19 +46,7 @@ function App() {
 
         <CarouselImages />
 
-        <Grid container spacing={2}>
-          {productsToShow.map((product) => {
-            return (
-              <Grid item xs={12} sm={6} md={4}>
-                <ProductCard
-                  product={product}
-                  key={product.id}
-                  onProductAdded={onProductAdded}
-                />
-              </Grid>
-            );
-          })}
-        </Grid>
+        <ProductsToShowOnSale />
         <Routes>
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
---
+++ src/components/cards/ProductsToShowOnSale.jsx
@@ -0,0 +1,31 @@
+import { Grid } from "@mui/material";
+import React from "react";
+import dataArray from "../ejemplo";
+import useAccountant from "../hooks/useAccountant";
+import ProductCard from "./ProductCard";
+
+const ProductsToShowOnSale = () => {
+  const productsToShow = dataArray.filter((card) => card.sale);
+  const { increase } = useAccountant(0);
+
+  const onProductAdded = (quantity) => {
+    increase(quantity);
+  };
+  return (
+    <Grid container spacing={2}>
+      {productsToShow.map((product) => {
+        return (
+          <Grid item xs={12} sm={6} md={4}>
+            <ProductCard
+              product={product}
+              key={product.id}
+              onProductAdded={onProductAdded}
+            />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+};
+
+export default ProductsToShowOnSale;
